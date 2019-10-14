262d4e745109b3a4d22b29ba79345c7ee7af9aa7
sachinsuresh <sachinsuresh00@gmail.com>
2019-10-14 20:09:10 +0530

    Day-3 with django

--- library/libraryapp/admin.py
+++ library/libraryapp/admin.py
@@ -4,7 +4,8 @@ from django.contrib import admin
 from .models import *
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('book_name', 'book_price', 'available', 'isbn', 'author', )
+    list_display = ('book_name', 'book_price', 'available', 'isbn', 'author', 'stock',)
+    ordering = ('book_name',)
     list_filter = ('available',)
     search_fields = ['book_name', 'isbn',]
     # fields = ('book_name', 'isbn', 'book_price')
@@ -14,5 +15,17 @@ admin.site.register(Book, BookAdmin)
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name',)
+    ordering = ('first_name', )
 
 admin.site.register(Author, AuthorAdmin)
+
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('member_name', 'member_phone', 'member_address')
+
+admin.site.register(Member, MemberAdmin)
+
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ('book', 'borrow_date', 'return_date', 'borrower_name', 'returned')
+
+admin.site.register(Record, RecordAdmin)
--- library/libraryapp/models.py
+++ library/libraryapp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import pre_save, post_save
 
 # Create your models here.
 class Author(models.Model):
@@ -14,11 +16,63 @@ class Book(models.Model):
     available = models.BooleanField()
     isbn = models.CharField(max_length = 20)
     author = models.ForeignKey(Author, null=True, on_delete=models.CASCADE)
+    stock = models.IntegerField(null = True, default = 1)
 
     def __str__(self):
         return self.book_name
 
+class Member(models.Model):
+    member_name = models.CharField(null = True, blank = True, max_length = 50)
+    member_phone = models.IntegerField(null = True, blank = True)
+    member_address = models.CharField(null = True, blank = True, max_length = 200)
+
+    def __str__(self):
+        return self.member_name
+
+class Record(models.Model):
+    book = models.ForeignKey(Book, null = True, on_delete = models.CASCADE)
+    borrow_date = models.DateField()
+    return_date = models.DateField()
+    borrower_name = models.ForeignKey(Member, on_delete = models.CASCADE)
+    #returned_check = models.BooleanField(default = False)
+    returned = models.BooleanField(default = False)
+    def __str__(self):
+        return self.book.book_name
+
+@receiver(pre_save, sender = Record)
+def update_stock(sender, instance, **kwargs):
+    if instance.returned == False:
+        if any(Record.objects.filter(id=instance.id)):
+            if Record.objects.get(id=instance.id).returned == True:
+                instance.returned = True
+            else:
+                book = instance.book
+                if instance.returned == True:
+                    book.stock = book.stock + 1
+                if book.stock > 0:
+                    book.available = True
+                book.save()
 
 
+#     book = instance.book
+#     if instance.returned == False:
+#         book.stock = book.stock - 1
+#     else:
+#         book.stock = book.stock + 1
+#     if book.stock == 0:
+#         book.available = False
+#     else:
+#         book.available = True
+#     book.save()
 
-
+@receiver(post_save, sender = Record)
+def decrement_stock(sender, instance, created, **kwargs):
+    if created:
+        book = instance.book
+        if book.available == True:
+            book.stock = book.stock - 1
+            if book.stock < 1:
+                book.available = False
+            book.save()
+        else:
+            book = None
